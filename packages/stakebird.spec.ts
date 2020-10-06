a73063b42c7a2b41dd2c7338ae76d034d7a80b50
Shane Vitarana <shanev@gmail.com>
2020-10-05 20:23:57 -0400

    added stakebird test

---
+++ packages/stakebird.spec.ts
@@ -0,0 +1,18 @@
+import { fromHex } from "./encoding/src/hex"
+import { Secp256k1 } from "./crypto/src/secp256k1";
+
+describe("Stakebird", () => {
+  // test data generate using https://github.com/nym-zone/bech32
+  // bech32 -e -h eth 9d4e856e572e442f0a4b2763e72d08a0e99d8ded
+  const ethAddressRaw = fromHex("9d4e856e572e442f0a4b2763e72d08a0e99d8ded");
+
+  const privKey = ".....35d36d93bfef4d2ae287"
+  const privHexKey = fromHex(privKey)
+
+  describe("publickey", () => {
+    it("works", async () => {
+        const keypair = await Secp256k1.makeKeypair(privHexKey)
+        expect(keypair.pubkey).toEqual(fromHex("adsfasdfsf"))
+    });
+  });
+});
