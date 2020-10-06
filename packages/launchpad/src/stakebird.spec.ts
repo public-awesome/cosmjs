ea29623c3e3b52ded06883f33d33e27e15c9cb7c
Shane Vitarana <shanev@gmail.com>
2020-10-05 21:39:35 -0400

    derive bech32 encoded public key

--- packages/launchpad/src/stakebird.spec.ts
+++ packages/launchpad/src/stakebird.spec.ts
@@ -1,18 +1,18 @@
-import { fromHex } from "@cosmjs/encoding";
+import { fromHex, toHex, Bech32 } from "@cosmjs/encoding";
 import { Secp256k1 } from "@cosmjs/crypto";
 
 describe("Stakebird", () => {
-  // test data generate using https://github.com/nym-zone/bech32
-  // bech32 -e -h eth 9d4e856e572e442f0a4b2763e72d08a0e99d8ded
-  const ethAddressRaw = fromHex("9d4e856e572e442f0a4b2763e72d08a0e99d8ded");
-
-  const privKey = ".....35d36d93bfef4d2ae287";
+  const privKey = "b8c462d2bb0c1a92edf44f735021f16c270f28ee2c3d1cb49943a5e70a3c763e";
   const privHexKey = fromHex(privKey);
 
-  describe("publickey", () => {
+  describe("derive public key", () => {
     it("works", async () => {
       const keypair = await Secp256k1.makeKeypair(privHexKey);
-      expect(keypair.pubkey).toEqual(fromHex("adsfasdfsf"));
+      expect(keypair.pubkey).toEqual(fromHex("04f146c27639179e5b67b8646108f48e1a78b146c74939e34afaa5414ad5c93f8aec5404281a625026f93f56575c4a94d75f3a4ac5a2403feaf1c8ef24bd1c99c5"));
+
+      const uncompressed = keypair.pubkey
+      const encoded = Bech32.encode("stbpub", Secp256k1.compressPubkey(uncompressed));
+      expect(encoded).toEqual("stbpub1q0c5dsnk8yteukm8hpjxzz853cd83v2xcaynnc62l2j5zjk4eylc5xgs30m")
     });
   });
 });
